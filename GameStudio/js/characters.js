21a3763770ada5e9caceb620f5523388a50ba184
Leandro Luna <135192663+LeandroLuna22@users.noreply.github.com>
2026-02-16 19:34:03 -0300

    Repair
    Reparo, erro de tranferencia

--- GameStudio/js/characters.js
+++
@@ -1,8 +0,0 @@
-// characters.js
-
-export const characters = [
-    { name: "Azul", color: "blue", sprite: "/img/spritesheet1.png" },
-    { name: "Verde", color: "green", sprite: "/img/donkey-kong.png" },
-    { name: "Amarelo", color: "yellow", sprite: "/img/yellow.png" }
-];
-
